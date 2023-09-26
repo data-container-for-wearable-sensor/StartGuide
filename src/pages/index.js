eefc55ed1c8a1a5271cbe1a5f090babbe0aa2fd2
seiketkm <seike.takumi.999@tis.co.jp>
2023-09-26 18:51:43 +0900

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            導入📄
+            導入へ📄
           </Link>
         </div>
       </div>
